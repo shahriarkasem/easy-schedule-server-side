91f8fb883cf0be5e65917e434a00e65a52f03076
abrakib942 <abrakib942@gmail.com>
2022-08-02 11:52:48 +0600

    first

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
