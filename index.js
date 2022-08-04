3134060889402a4c63cf96920fb7c33cc2855046
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-08-04 11:43:37 +0600

    JWT

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
@@ -11,79 +12,105 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+//VerifyJWT
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bvzmv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 // connect with database
 async function run() {
-  try {
-    await client.connect();
-    const userCollection = client.db("userData").collection("users");
+    try {
+        await client.connect();
+        const userCollection = client.db("userData").collection("users");
 
-    // get all users
-    app.get("/users", async (req, res) => {
-      const query = {};
-      const users = userCollection.find(query);
-      const newUsers = await users.toArray();
-      res.send(newUsers);
-    });
+        //AUTH(JWT)
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
-    // post user
-    app.post("/users", async (req, res) => {
-      const newUser = req.body;
-      console.log("adding new user", newUser);
-      const result = await userCollection.insertOne(newUser);
-      res.send(result);
-    });
+        // get all users
+        app.get("/users", async (req, res) => {
+            const query = {};
+            const users = userCollection.find(query);
+            const newUsers = await users.toArray();
+            res.send(newUsers);
+        });
 
-    // find specific user by user's id
-    app.get("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await userCollection.findOne(query);
-      res.send(result);
-    });
+        // post user
+        app.post("/users", async (req, res) => {
+            const newUser = req.body;
+            console.log("adding new user", newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        });
 
-    // update user
-    app.put("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const updatedUser = req.body;
-      const query = { _id: ObjectId(id) };
-      const options = { upsert: true };
-      const updatedDoc = {
-        $set: {
-          name: updatedUser.name,
-          ages: updatedUser.ages,
-        },
-      };
-      const result = await userCollection.updateOne(query, updatedDoc, options);
-      res.send(result);
-    });
+        // find specific user by user's id
+        app.get("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        });
 
-    // delete user
-    app.delete("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await userCollection.deleteOne(query);
-      res.send(result);
-    });
+        // update user
+        app.put("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    ages: updatedUser.ages,
+                },
+            };
+            const result = await userCollection.updateOne(query, updatedDoc, options);
+            res.send(result);
+        });
 
-    //------------ / --------------
-  } finally {
-  }
+        // delete user
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        //------------ / --------------
+    } finally {
+    }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("EasySchedule server-side is working fine");
+    res.send("EasySchedule server-side is working fine");
 });
 
 app.listen(port, () => {
-  console.log("EasySchedule app is listening on port", port);
+    console.log("EasySchedule app is listening on port", port);
 });
 
 // front end server api
