9ceee40aeb3c6fbcef0e54620c8619c25af439d9
JabedH <jabedhtusar@gmail.com>
2022-09-05 13:17:36 +0600

    added

--- index.js
+++ index.js
@@ -37,21 +37,21 @@ const client = new MongoClient(uri, {
 });
 
 //VerifyJWT
-function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden access" });
-    }
-    console.log("decoded", decoded);
-    req.decoded = decoded;
-    next();
-  });
-}
+// function verifyJWT(req, res, next) {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "unauthorized access" });
+//   }
+//   const token = authHeader.split(" ")[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(403).send({ message: "Forbidden access" });
+//     }
+//     console.log("decoded", decoded);
+//     req.decoded = decoded;
+//     next();
+//   });
+// }
 
 // connect with database
 async function run() {
